cdcd2a032fd939415fbb5633ee9a8442853edb5f
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2021-08-18 09:37:32 +0300

    123

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express()
 const port = 4000;
+
+
+app.get('/', (request, response) => {
+    console.log(`URL: ${request.url}`);
+    response.send('Hello, Server!');
+});
+
+
+
+
+
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+ 
+    console.log(`Server listening on port ${server.address().port}`);
+});
+
