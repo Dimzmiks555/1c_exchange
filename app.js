6e611bfe57714ec364cfc4de9173f9bca7bbe22d
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2021-08-18 14:17:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const app = express()
 const port = 4000;
 const base64 = require('base-64');
 
-let username = 'Администратор';
-let password = '2013';
+
 
 // let headers = { 'Authorization': 'Basic ' + base64.encode(username + ":" + password) };
 let headers = { 'Authorization': 'Basic 0JDQtNC80LjQvdC40YHRgtGA0LDRgtC+0YA6MjAxMw=='}
@@ -16,24 +15,32 @@ app.get('/', (request, response) => {
 });
 
 
-
-let uri = 'http://server/1CBase/odata/standard.odata/Catalog_Номенклатура?$top=10&$format=json';
-
-
-const encodedURI = encodeURI(uri); 
+let uriNom = 'http://server/1CBase/odata/standard.odata/Catalog_Номенклатура?$top=10000&$format=json';
+let uriPrice = 'http://server/1CBase/odata/standard.odata/InformationRegister_ЦеныНоменклатуры?$top=10000&$format=json';
+let uriAmount = 'http://server/1CBase/odata/standard.odata/AccumulationRegister_ТоварыНаСкладах?$top=10000&$format=json';
 
 
-fetch(encodedURI, {
+const NomEncodedURI = encodeURI(uriNom); 
+const PriceEncodedURI = encodeURI(uriPrice); 
+const AmountEncodedURI = encodeURI(uriAmount);
+
+fetch(NomEncodedURI, {
+    
     method:'GET',
     headers: headers,
-    }
-)
+
+})
 .then(res => res.json())
 .then(json => {
 
-    let nomenklatura = []
+    let data = json.value
 
-    json.value.forEach(item => {
+    let filteredData = data.filter(item => item['IsFolder'] != true)
+
+    let nomenklatura = [];
+
+    filteredData.forEach(item => {
+
         nomenklatura.push(
             {
                 'title': item['НаименованиеПолное'],
@@ -44,16 +51,76 @@ fetch(encodedURI, {
                 'type': item['ВидНоменклатуры_Key'],
                 'unit': item['ЕдиницаИзмерения_Key'],
                 'country': item['СтранаПроисхождения_Key'],
-                
+                'parent_key' : item['Parent_Key']
             }
         )
     });
-    console.log(nomenklatura);
+    console.log(nomenklatura.length);
 })
 .catch(err => {
     console.log(err)
 })
 
+fetch(PriceEncodedURI, {
+    
+    method:'GET',
+    headers: headers,
+
+})
+.then(res => res.json())
+.then(json => {
+
+    let prices = [];
+
+    json.value.forEach(item => {
+        prices.push(...item['RecordSet'])
+    })
+
+    prices.forEach(item => {
+        let date = new Date(item["Period"]);
+        item.Fulldate = date.getTime()
+    })
+
+    prices.sort((a, b) => {
+        return b.Fulldate - a.Fulldate;
+    })
+
+
+
+    console.log(prices.length)
+})
+.catch(err => {
+    console.log(err)
+})
+
+fetch(AmountEncodedURI, {
+    
+    method:'GET',
+    headers: headers,
+
+})
+.then(res => res.json())
+.then(json => {
+    let amounts = [];
+
+    json.value.forEach(item => {
+        amounts.push(...item['RecordSet'])
+    })
+
+    amounts.forEach(item => {
+        let date = new Date(item["Period"]);
+        item.Fulldate = date.getTime()
+    })
+
+    amounts.sort((a, b) => {
+        return b.Fulldate - a.Fulldate;
+    })
+
+    console.log(amounts.length)
+})
+.catch(err => {
+    console.log(err)
+})
 
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
